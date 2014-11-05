f5732566fcc4149b0c924b89cab6790b772f5801
ilya.kucheruavyu <kojiba@ro.ru>
2014-11-05 13:21:50 +0200

    sandbox forks fully 2

---
+++ Classes/RayFoundation/RayBase.c
@@ -0,0 +1,23 @@
+#include "RayBase.h"
+
+void* mallocFunc(size_t size) {
+#if RAY_SHORT_DEBUG == 1
+    printf("Ray malloc hook\n");
+#endif
+    return RMallocPtr(size);
+}
+
+void freeFunc(void *pointer) {
+#if RAY_SHORT_DEBUG == 1
+    printf("Ray free hook\n");
+#endif
+    return RFreePtr(pointer);
+}
+
+void initPointers() {
+#if RAY_SHORT_DEBUG == 1
+    printf("Init Ray Pointers\n");
+#endif
+    RMallocPtr = RTrueMalloc;
+    RFreePtr = RTrueFree;
+}
