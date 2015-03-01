83391a622527e430348ebb139affa31ded48b783
kojiba <kojiba@ro.ru>
2015-03-01 23:26:47 +0200

    Removed error, but threadIdentifier not currently works on windows.

--- Classes/RayFoundation/RayConfig.h
+++ Classes/RayFoundation/RayConfig.h
@@ -31,12 +31,7 @@
 #define RAY_POOL_THREAD_SAFE
 #define RAY_SAND_BOX_THREAD_SAFE
 
-//#define R_POOL_DETAILED      // enable to storage allocator thread id and malloced size
-
-#if defined(__WIN32) && defined(R_POOL_DETAILED)
-    #error NOT AVAILABLE
-#else
-#endif
+#define R_POOL_DETAILED      // enable to storage allocator thread id and malloced size
 
 //#define RAY_ASSERT_ON_ERRORS // enables assert on errors
 #ifdef RAY_ASSERT_ON_ERRORS
